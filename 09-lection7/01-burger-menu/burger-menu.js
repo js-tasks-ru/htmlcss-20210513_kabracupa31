88508d70998391b4f13de7f457671e4eccd8e0e0
Olga Trotsenko <kabracupa31@gmail.com>
2021-07-12 22:40:06 +0300

    hide animation on page load

--- 09-lection7/01-burger-menu/burger-menu.js
+++ 09-lection7/01-burger-menu/burger-menu.js
@@ -1,19 +1,29 @@
 const menu = document.getElementById('burger-menu');
 const menuClose = document.getElementById('burger-menu-close');
 const menuOpen = document.getElementById('burger-menu-open');
+//const mainBody = document.getElementsByTagName("body")[0];
 
 if (menuOpen) {
     menuOpen.addEventListener('click', function() {
         if (menu) {
-            menu.setAttribute('data-open', '');
+            //if(document.documentElement.getAttribute("animation-on")){
+            //    mainBody.classList.remove("preload");   
+            //}
+            menu.setAttribute('data-open', '');  
+            
         }
     });
 }
-
 if (menuClose) {
     menuClose.addEventListener('click', function() {
+        
         if (menu) {
-            menu.removeAttribute('data-open');
+            menu.removeAttribute('data-open');  
+            //if(document.documentElement.getAttribute("animation-on")){
+            //    setTimeout(function () {mainBody.classList.add("preload"); }, 300);   
+            //}   
         }
+       
     });
+    
 }
